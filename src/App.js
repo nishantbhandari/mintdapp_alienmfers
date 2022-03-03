457a43a9c0a8974393210a47a98563fffb14ced5
nishantbhandari <bhandari.nishant@gmail.com>
2022-03-03 22:13:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -398,7 +398,7 @@ function App() {
               }}
             >
 
-3000 alien mfers dropping in the alienverse | 50% gains for Ukraine Relief 🇺🇦 | all hail @sartoshi_nft | MINT Price: 0.0069 ETH + GAS | MINTING LIVE!            </s.TextTitle>
+3000 alien mfers dropping in the alienverse.  50% gains for Ukraine Relief 🇺🇦. all hail @sartoshi_nft             </s.TextTitle>
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
