55e3a72aafdd0e4a0425f25a51044a82b02b373a
nishantbhandari <bhandari.nishant@gmail.com>
2022-03-05 00:25:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -398,7 +398,8 @@ function App() {
           </s.Container>
           <s.SpacerLarge />
           <s.Container flex={1} jc={"center"} ai={"center"}>
-         
+                     <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
